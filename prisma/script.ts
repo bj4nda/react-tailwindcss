3e6eb14457ca2c1bdbe7ab928a892f66b3229226
b4jnda <bharath4jinda@gmail.com>
2023-02-21 01:34:07 +0530

    initial commit

---
+++ prisma/script.ts
@@ -0,0 +1,23 @@
+import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
+
+async function main() {
+
+
+    /* const user = await prisma.user.findFirst(
+        {
+            where: {
+               
+                    name: "Shruti"
+            },
+        }
+    ) */
+    
+    
+}
+
+main().
+catch((e) => console.error(e.message))
+.finally(async() => {
+    await prisma.$disconnect()
+})
